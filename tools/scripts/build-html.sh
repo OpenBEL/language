62d6b9c2738e341c3d6af0e59f4c9d0fa3c77f6e
Anthony Bargnesi <abargnesi@selventa.com>
2015-10-19 21:25:48 -0400

    added initial port of BEL v2.0 wiki specification

--- tools/scripts/build-html.sh
+++ tools/scripts/build-html.sh
@@ -3,3 +3,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
 
 "$DIR"/tools/asciidoc/asciidoc.py -b html5 -a toc2 -a theme=flask \
     version_1.0/bel_specification_version_1.0.adoc
+
+"$DIR"/tools/asciidoc/asciidoc.py -b html5 -a toc2 -a theme=flask \
+    version_2.0/bel_specification_version_2.0.adoc

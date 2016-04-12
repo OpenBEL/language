201c6b5e94c27e9f15c73a0344cdd7177ab69cb1
William Hayes <william.s.hayes@gmail.com>
2016-04-12 14:02:55 -0400

    Updated asciidoctor build script

--- tools/scripts/build-using-asciidoctor.sh
+++ tools/scripts/build-using-asciidoctor.sh
@@ -1,44 +1,16 @@
 #!/usr/bin/env bash
 
-asciidoctor -a stylesheet=../tools/asciidoctor-stylesheets/foundation.css version_1.0/bel_specification_version_1.0.adoc
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-    # error with v1.0
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    echo "| Error rendering HTML for BEL v1.0 specification! |"
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-fi
+set -e
 
+TOP="$(pwd)"
 
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-    # error with v2.0
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    echo "| Error rendering HTML for BEL v2.0 specification! |"
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-fi
+cp tools/asciidoctor-stylesheets/openbel-custom.css version_2.0
+cp tools/asciidoctor-stylesheets/openbel-custom.css version_1.0
 
-asciidoctor-pdf version_1.0/bel_specification_version_1.0.adoc
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-    # error with pdf for v1.0
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    echo "| Error rendering PDF for BEL v1.0 specification! |"
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-fi
+cd $TOP/version_1.0
+asciidoctor -a stylesheet=openbel-custom.css bel_specification_version_1.0.adoc
+asciidoctor-pdf bel_specification_version_1.0.adoc
 
-asciidoctor-pdf version_2.0/bel_specification_version_2.0.adoc
-EXIT_CODE=$?
-if [ $EXIT_CODE -ne 0 ]; then
-    # error with pdf for v1.0
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-    echo "| Error rendering PDF for BEL v2.0 specification! |"
-    echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-fi
-
-exit $EXIT_CODE
-
-
-
-# New code to convert to html: 
-# asciidoctor -a stylesheet=openbel-custom.css -a stylesdir=. bel_specification_version_2.0.adoc 
+cd $TOP/version_2.0
+asciidoctor -a stylesheet=openbel-custom.css bel_specification_version_2.0.adoc
+asciidoctor-pdf bel_specification_version_2.0.adoc

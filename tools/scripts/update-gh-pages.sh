af07fe38f8ada92a14f1fd91d477d4540c28d59a
Nick Bargnesi <nbargnesi@selventa.com>
2016-04-12 15:24:09 -0400

    bump gh-pages update

--- tools/scripts/update-gh-pages.sh
+++ tools/scripts/update-gh-pages.sh
@@ -5,21 +5,24 @@ dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
 cd "$dir" || exit 1
 
 # What's our current rev?
-cur=$(git rev-parse HEAD)
+cur=$(git rev-parse HEAD) || exit 1
 
 # Clone the current GitHub repo
-git clone $(git config --get remote.origin.url language)
+git clone $(git config --get remote.origin.url language) || exit 1
 
 # Hard reset to $cur on the clone (in case of fast-forwards)
 cd language || exit 1
-git reset --hard $cur
+git reset --hard $cur || exit 1
 
 # Switch branches
-git checkout gh-pages
+git checkout gh-pages || exit 1
 
 # Automerge
-git merge master -m "OpenBEL Build Server gh-pages autoupdate"
+git merge master -m "OpenBEL Build Server gh-pages autoupdate" || exit 1
 
 # And push
-git push origin gh-pages
+git push origin gh-pages || exit 1
 
+# Cleanup, back to top of tree
+cd "$dir" || exit 1
+rm -fr "language"
